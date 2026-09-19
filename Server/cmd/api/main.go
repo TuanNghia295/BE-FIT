c241a3254e780f27c65fdef0ea9c7ec310f8bc99
TuanNghia <tuannghiait2905@gmail.com>
2026-09-19 19:14:21 +0700

    refactor main.go to improve server startup logic and enhance readability

--- Server/cmd/api/main.go
+++ Server/cmd/api/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/TuanNghia295/BE-FIT/package/database"
 	"github.com/joho/godotenv"
@@ -15,4 +18,12 @@ func main() {
 	}
 	// Connect to the database
 	database.ConnectDB()
+
+	port := os.Getenv("PORT")
+	// Start API server
+	fmt.Printf("BE-FIT API running on: http://localhost:%s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
